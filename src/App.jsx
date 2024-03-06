f3cad3dac7c00d2a2a3b1301c28b5c2f1d78efa4
Alex Dang m1 <lesadang1@gmail.com>
2024-03-06 11:35:24 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log(2);
+  console.log(3);
   return (
     <>
       <div>
-        <a href='https://vitejs.dev' target="_blank">
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
+        <a href='https://react.dev' target="_blank">
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
