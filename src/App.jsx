0986d99a550eeec1a4d4f34af5b5b2f1740a4e16
LisaL <lalopez6918@gmail.com>
2024-04-02 07:57:40 -0700

    front-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <button className='btn btn-primary'>Test button</button>
+      <Header />
+
+      <div className='banner'></div>
+      <Footer />
     </>
   );
 }
